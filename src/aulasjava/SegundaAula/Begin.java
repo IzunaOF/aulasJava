32922f2f6bcc2e84dbffc5963aead7611ee13ef7
LEONARDO DOS SANTOS <leonardo_santos29@estudante.sesisenai.org.br>
2023-07-17 19:03:15 -0300

    inicializando uma classe

--- src/aulasjava/SegundaAula/Begin.java
+++ src/aulasjava/SegundaAula/Begin.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package aulasjava.SegundaAula;
+package aulasJava;
 
-/**
- *
- * @author leonardo_santos29
- */
+import javax.swing.JOptionPane;
+
 public class Begin {
-    
+
+    public static void main(String[] args) {
+        String[] letras = {"A", "a", "z", "t", "e", "y", "u"};
+        String[] vogais = {"a", "e", "i", "o", "u"};
+
+        for (String letra : letras) {
+            for (String vogal : vogais) {
+
+                String character = letra.toLowerCase();
+
+                if (character.equals(vogal)) {
+                    System.out.println("A letra " + letra + " é uma vogal");
+                    System.out.println("A proxima letra no alfabeto é " + getChar(letra));
+                    break;
+                } else {
+                    System.out.println("A letra " + letra + " é uma consoante");
+                    System.out.println("A proxima letra no alfabeto é " + getChar(letra));
+                    break;
+                }
+
+            }
+
+        }
+
+        double[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        double valorSomado = soma(nums);
+
+        System.out.println("O valor total do vetor de números somado é " + valorSomado);
+    }
+
+     static char getChar(String letra) {
+        char character = ' ';
+        for (int i = 0; i < 127; i++) {
+            if (letra.charAt(0) == i) {
+                character = (char) (i + 1);
+                break;
+            }
+        }
+        return character;
+    }
+
+    static double soma(double[] vetor) {
+        double result = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            result += vetor[i];
+        }
+        return result;
+    }
 }
--- src/aulasjava/SegundaAula/Momento2.java
+++ src/aulasjava/SegundaAula/Momento2.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package aulasjava.SegundaAula;
 
-/**
- *
- * @author leonardo_santos29
- */
+package aulasJava;
+
+import java.util.ArrayList;
+
 public class Momento2 {
-    
+
+    public static void main(String[] args) {
+        char[] respostas = {'M', 'N', 'V', 'N', 'V'};
+
+        for (char opt : respostas) {
+            switch (opt) {
+                case 'M':
+                    System.out.println("Bom dia!");
+                    break;
+                case 'V':
+                    System.out.println("Bom Tarde!");
+                    break;
+                case 'N':
+                    System.out.println("Bom Noite!");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+
+        double[] meuVetor = {12, 11, 17, 54.5, 54.3, 37, 74, 97};
+        bubbleSort(meuVetor, true);
+    }
+
+    static void bubbleSort(double[] vetor, boolean cresc) {
+        double[] vetorAux = vetor.clone();
+        double aux = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[i] > vetor[j]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor[j] = aux;
+                }
+                if (vetorAux[i] < vetorAux[j]) {
+                    aux = vetorAux[i];
+                    vetorAux[i] = vetorAux[j];
+                    vetorAux[j] = aux;
+                }
+            }
+        }
+        
+        if(!cresc){
+            vetor = vetorAux;
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+
+    }
 }
