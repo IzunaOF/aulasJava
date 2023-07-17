4ccc2748b26c56fb1eac1e7333a619593d1e5db9
LEONARDO DOS SANTOS <leonardo_santos29@estudante.sesisenai.org.br>
2023-07-17 20:34:21 -0300

    entradas Scanner

--- src/aulasjava/Aula03/Aula03.java
+++ src/aulasjava/Aula03/Aula03.java
@@ -1,8 +1,24 @@
 package aulasjava.Aula03;
 
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class Aula03 {
     public static void main(String[] args){
-         JOptionPane.showMessageDialog(null, "anos de idade");
+        
+         Scanner leitor = new Scanner(System.in);
+         System.out.println("Informe Seu Nome: ");
+         String nome = leitor.nextLine();
+         
+         System.out.println("Você é estudante? ");
+         Boolean estudante = leitor.nextBoolean();
+         
+         System.out.println("Digite a média das notas: ");
+         Double media = leitor.nextDouble();
+         
+         System.out.println("Quantos anos você tem? ");
+         Integer idade = leitor.nextInt();
+         //JOptionPane.showMessageDialog(null, "Bem Vindo " + nome);
+         System.out.println("Olá " + nome);
     }
 }
